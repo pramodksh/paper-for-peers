cdc3eb3750b934b618821585f8d127c7ea7fc271
pramodksh <pramodkumarsh3@gmail.com>
2021-05-20 00:26:48 +0530

    Made Widgets Responsive

--- lib/module/login/login.dart
+++ lib/module/login/login.dart
@@ -9,6 +9,7 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool _isLogIn = true;
 
   Widget getOrDivider() => Row(
         children: <Widget>[
@@ -30,116 +31,137 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-          fit: BoxFit.cover,
-          colorFilter: ColorFilter.mode(
-              Colors.black.withOpacity(0.2), BlendMode.dstATop),
-          image: AssetImage(
-            'assets\\images\\appBackground.png',
-          ),
-        )),
-        child: Container(
-          child: SingleChildScrollView(
-            child: Container(
-              margin: EdgeInsets.fromLTRB(55, 70, 55, 30),
-              width: MediaQuery.of(context).size.width,
-              // color: Colors.red,
-              child: Column(
-                children: [
-                  Image.asset(
-                    'assets\\logo\\mainLogo.png',
-                    height: 110,
-                    alignment: Alignment.center,
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    'Sign Up',
-                    style: GoogleFonts.ptSans(
-                        fontSize: 35,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.black38),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  customTextField(inputBoxText: 'Email Address'),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  customTextField(inputBoxText: 'Password', obscureText: true),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  customTextField(
-                      inputBoxText: 'Confirm Password', obscureText: true),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  SizedBox(
-                    width: 350,
-                    height: 50,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.black12),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(28.0),
-                                    side: BorderSide(
-                                        color: Colors.transparent,
-                                        width: 200
-                                    )
-                                )
-                            ),
-                      ),
-                      child: Text(
-                        'Sign Up',
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w400,
-                            color: Colors.black),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 35,
-                  ),
-                  getOrDivider(),
+    print("LOGIN? ${_isLogIn}");
 
-                  TextButton(
-                    onPressed: () {},
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          'assets\\logo\\google-icon.png',
-                          height: 30,
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.2), BlendMode.dstATop),
+              image: AssetImage('assets\\images\\appBackground.png',),
+            ),
+          ),
+          child: Container(
+            margin: EdgeInsets.fromLTRB(55, 70, 55, 30),
+            width: MediaQuery.of(context).size.width,
+            // color: Colors.red,
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets\\logo\\mainLogo.png',
+                  height: 110,
+                  alignment: Alignment.center,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  _isLogIn ? 'Sign In' : 'Sign Up',
+                  style: GoogleFonts.ptSans(
+                      fontSize: 35,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black38),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                customTextField(inputBoxText: 'Email Address'),
+                SizedBox(
+                  height: 20,
+                ),
+                customTextField(inputBoxText: 'Password', obscureText: true),
+                SizedBox(
+                  height: 10,
+                ),
+                _isLogIn
+                    ? Container()
+                    : customTextField(
+                        inputBoxText: 'Confirm Password', obscureText: true),
+                _isLogIn ?  Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      child: Text('Forgot Password',
+                        style: TextStyle(
+                          color: Colors.black38
                         ),
-                        Divider(
-                          height: 15,
-                          color: Colors.red,
-                        ),
-                        SizedBox(
-                          width: 15,
-                        ),
-                        Text(
-                          'Continue from Google',
-                          style: TextStyle(color: Colors.black),
-                        ),
-                      ],
+                      ),
+                      onPressed: (){},
                     ),
-                  )
-                ],
-              ),
+                  ],
+                ):Container() ,
+                _isLogIn ? Container():SizedBox(height: 40,) ,
+                SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.black12),
+                      shape:
+                          MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(28.0),
+                                  side: BorderSide(
+                                      color: Colors.transparent,
+                                      width: 200))),
+                    ),
+                    child: Text(
+                      _isLogIn ? "Sign In" : 'Sign Up',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 35,),
+                getOrDivider(),
+                TextButton(
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets\\logo\\google-icon.png',
+                        height: 30,
+                      ),
+                      Divider(
+                        height: 15,
+                        color: Colors.red,
+                      ),
+                      SizedBox(
+                        width: 15,
+                        height: 50,
+                      ),
+                      Text(
+                        'Continue from Google',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _isLogIn = !_isLogIn;
+                    });
+                  },
+                  child: Text(
+                    _isLogIn
+                        ? 'Already a Member? Sign In'
+                        : "New Member ? Create Account",
+                  ),
+                ),
+              ],
             ),
           ),
         ),
