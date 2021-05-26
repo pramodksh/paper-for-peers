22721ef2c401ef3d48e254f010692ef2869fba58
akash <akash.punagin@gmail.com>
2021-05-26 21:28:06 +0530

    main dashboard - bottom navigation bar done

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -2,4 +2,7 @@ import 'package:flutter/material.dart';
 
 class CustomColors {
  static const Color backGroundColor = Color(0xff0D182A);
+ static const Color bottomNavBarColor = Color(0xff142A48);
+ static const Color bottomNavBarSelectedIconColor = Color(0xffDCDCDC);
+ static const Color bottomNavBarUnselectedIconColor = Color(0xff9A9A9A);
 }
--- lib/modules/dashboard/main_dashboard.dart
+++ lib/modules/dashboard/main_dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:papers_for_peers/config/export_config.dart';
 
 class MainDashboard extends StatefulWidget {
@@ -7,6 +8,18 @@ class MainDashboard extends StatefulWidget {
 }
 
 class _MainDashboardState extends State<MainDashboard> {
+
+  int selectedItemPosition = 1;
+  final double bottomNavBarRadius = 20;
+  final double bottomNavBarHeight = 90;
+
+  List<Map> bottomNavBarIcons = [
+    {"icon": AssetImage(DefaultAssets.questionPaperNavIcon,), "label": "Question Paper"},
+    {"icon": AssetImage(DefaultAssets.notesNavIcon,), "label": "Notes"},
+    {"icon": AssetImage(DefaultAssets.journalNavIcon,), "label": "Journal"},
+    {"icon": AssetImage(DefaultAssets.syllabusCopyNavIcon,), "label": "Syllabus Copy"},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +44,71 @@ class _MainDashboardState extends State<MainDashboard> {
           ),
           SizedBox(width: 5,),
         ],
+      ),
+      body: Builder(
+        builder: (context) {
+          if (selectedItemPosition == 0) {
+            return Text("ZERO");
+          } else if (selectedItemPosition == 1) {
+            return Text("one");
+          } else if (selectedItemPosition == 2) {
+            return Text("TWO");
+          } else {
+            return Text("ELSE");
+          }
+        },
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: CustomColors.bottomNavBarColor,
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(bottomNavBarRadius),
+            topLeft: Radius.circular(bottomNavBarRadius),
+          ),
+        ),
+        // height: bottomNavBarHeight,
+        height: 75,
+        child: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: List.generate(
+                bottomNavBarIcons.length,
+                (index) => SizedBox(
+                  width: MediaQuery.of(context).size.width / 4,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Transform.scale(
+                        scale: 1.7,
+                        child: IconButton(
+                          padding: EdgeInsets.zero,
+                          // constraints: BoxConstraints(),
+                          onPressed: () {
+                            setState(() {
+                              selectedItemPosition = index;
+                            });
+                          },
+                          icon: ImageIcon(
+                            bottomNavBarIcons[index]["icon"],
+                            color: selectedItemPosition == index
+                                ? CustomColors.bottomNavBarSelectedIconColor
+                                : CustomColors.bottomNavBarUnselectedIconColor,
+                          ),
+                        ),
+                      ),
+                      selectedItemPosition == index ? Text(
+                        bottomNavBarIcons[index]["label"],
+                        style: TextStyle(
+                          color: CustomColors.bottomNavBarSelectedIconColor,
+                          fontSize: 12,
+                        ),
+                      ) : Container(),
+                    ],
+                  ),
+                )
+            ),
+          ),
+        ),
       ),
     );
   }
