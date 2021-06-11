a9eb815e2ae5bd9180a642c626f4d1cd574484ed
akash <akash.punagin@gmail.com>
2021-06-11 14:09:52 +0530

    merged profile branch

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,6 @@ import 'package:papers_for_peers/modules/login/user_details.dart';
 // import 'package:papers_for_peers/modules/login/welcome_message.dart';
 import 'package:papers_for_peers/modules/testing_screens/clip_background.dart';
 import 'package:papers_for_peers/modules/dashboard/notifications/notifications.dart';
-import 'file:///C:/Users/pramod/AndroidStudioProjects/paper-for-peers-new/lib/modules/login/welcom_screen.dart';
-import 'package:papers_for_peers/modules/testing_screens/sliding_screens.dart';
 import 'package:papers_for_peers/modules/testing_screens/image_picker.dart';
 import 'package:papers_for_peers/services/theme_provider/theme_provider.dart';
 import 'package:provider/provider.dart';
