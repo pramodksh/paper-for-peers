9d5b4a5da5789a55a2e891c52043ce06b5e114a3
pramodksh <pramodkumarsh3@gmail.com>
2021-06-11 11:54:47 +0530

    Added user_details.dart file

---
+++ lib/modules/login/user_details.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+import '../../config/default_assets.dart';
+import '../../config/export_config.dart';
+import 'utilities.dart';
+import 'utilities.dart';
+
+class UserDetails extends StatefulWidget {
+  @override
+  _UserDetailsState createState() => _UserDetailsState();
+}
+
+class _UserDetailsState extends State<UserDetails> {
+  double borderThickness = 5;
+  double profileImageRadius = 90;
+
+  Widget getCircularProfileImage({@required imagePath}) {
+    return Container(
+      padding: EdgeInsets.all(borderThickness),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        gradient: LinearGradient(
+          begin: Alignment.centerRight,
+          end: Alignment.centerLeft,
+          // stops: [0.1, 0.5, 0.7, 0.9],
+          colors: [
+            Color(0xff6A0EB1),
+            Color(0xff8C2196),
+          ],
+        ),
+      ),
+      child: CircleAvatar(
+        radius: profileImageRadius,
+        backgroundImage: AssetImage(imagePath),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.black.withOpacity(0.1),
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.2), BlendMode.dstATop),
+              image: AssetImage(DefaultAssets.appBackgroundPath),
+            ),
+          ),
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SingleChildScrollView(
+            child: Container(
+              width: double.infinity,
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 100,
+                  ),
+                  Stack(
+                    children: [
+                      getCircularProfileImage(
+                          imagePath: DefaultAssets.profileImagePath),
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Container(
+                          padding: EdgeInsets.all(6),
+                          decoration: BoxDecoration(
+                              color: CustomColors.bottomNavBarColor,
+                              shape: BoxShape.circle),
+                          child: IconButton(
+                              icon: Icon(
+                                Icons.camera_alt_outlined,
+                                color: Colors.white,
+                                size: 30,
+                              ),
+                              onPressed: () {}),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 80,
+                  ),
+                  Text(
+                    'Enter your Name',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                      margin: EdgeInsets.symmetric(horizontal: 70),
+                      child: getCustomTextField()
+                  ),
+                  // getCustomButton(buttonText: 'Continue', onPressed: (){})
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
