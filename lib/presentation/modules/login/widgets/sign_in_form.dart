012c3823a6c4a78d172a4c46edc37522d7593417
akash <akash.punagin@gmail.com>
2021-11-17 00:10:28 +0530

    added bloc listener for sign in and sign up

--- lib/presentation/modules/login/widgets/sign_in_form.dart
+++ lib/presentation/modules/login/widgets/sign_in_form.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:papers_for_peers/config/export_config.dart';
 import 'package:papers_for_peers/logic/cubits/sign_in/sign_in_cubit.dart';
 import 'package:papers_for_peers/logic/cubits/sign_up/sign_up_cubit.dart';
+import 'package:papers_for_peers/presentation/modules/dashboard/utilities/dialogs.dart';
 import 'package:papers_for_peers/presentation/modules/login/forgot_password.dart';
 import 'package:papers_for_peers/presentation/modules/login/utilities.dart';
 import 'package:papers_for_peers/presentation/modules/login/widgets/login_utils.dart';
@@ -16,73 +18,80 @@ class SignInForm extends StatelessWidget {
 
     SignInState signInState = context.watch<SignInCubit>().state;
 
-    return Container(
-      margin: EdgeInsets.fromLTRB(55, 70, 55, 30),
-      width: MediaQuery.of(context).size.width,
-      // color: Colors.red,
-      child: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            Image.asset(
-              DefaultAssets.mainLogoPath,
-              height: 110,
-              alignment: Alignment.center,
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            SizedBox(height: 20,),
-            getCustomTextField(
-              labelText: 'Email Address',
-              onChanged: (val) {
-                context.read<SignInCubit>().emailChanged(val);
-              },
-              validator: (String? val) => context.read<SignInCubit>().isEmailValid(val!) ? null : "Please enter valid email",
-            ),
-            SizedBox(height: 20,),
-            getCustomPasswordField(
-              inputBoxText: 'Password',
-              onChanged: (val) {
-                context.read<SignInCubit>().passwordChanged(val);
-              },
-              obscureText: signInState.isPasswordObscure,
-              onTapObscure: () { context.read<SignInCubit>().togglePasswordObscure(); },
-              validator: (String? val) => context.read<SignInCubit>().isPasswordValid(val!) ? null : "Enter Password",
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+    return BlocListener<SignInCubit, SignInState>(
+      listener: (context, state) {
+        if (state.signInStatus == SignInStatus.error) {
+          showAlertDialog(context: context, text: state.errorMessage);
+        }
+      },
+      child: Container(
+          margin: EdgeInsets.fromLTRB(55, 70, 55, 30),
+          width: MediaQuery.of(context).size.width,
+          // color: Colors.red,
+          child: Form(
+            key: _formKey,
+            child: Column(
               children: [
-                TextButton(
-                  child: Text('Forgot Password?', style: TextStyle(color: Colors.white, fontSize: 18),),
-                  onPressed: (){
-                    // todo implement forgot password
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => ForgotPassword(),
-                    ));
+                Image.asset(
+                  DefaultAssets.mainLogoPath,
+                  height: 110,
+                  alignment: Alignment.center,
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                SizedBox(height: 20,),
+                getCustomTextField(
+                  labelText: 'Email Address',
+                  onChanged: (val) {
+                    context.read<SignInCubit>().emailChanged(val);
                   },
+                  validator: (String? val) => context.read<SignInCubit>().isEmailValid(val!) ? null : "Please enter valid email",
+                ),
+                SizedBox(height: 20,),
+                getCustomPasswordField(
+                  inputBoxText: 'Password',
+                  onChanged: (val) {
+                    context.read<SignInCubit>().passwordChanged(val);
+                  },
+                  obscureText: signInState.isPasswordObscure,
+                  onTapObscure: () { context.read<SignInCubit>().togglePasswordObscure(); },
+                  validator: (String? val) => context.read<SignInCubit>().isPasswordValid(val!) ? null : "Enter Password",
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      child: Text('Forgot Password?', style: TextStyle(color: Colors.white, fontSize: 18),),
+                      onPressed: (){
+                        // todo implement forgot password
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => ForgotPassword(),
+                        ));
+                      },
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20,),
+
+                SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: signInState.signInStatus == SignInStatus.loading
+                    ? Center(child: CircularProgressIndicator.adaptive())
+                    : getCustomButton(
+                        buttonText: 'Sign in',
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            context.read<SignInCubit>().signInWithEmailAndPassword();
+                          }
+                        },
+                    ),
                 ),
               ],
             ),
-            SizedBox(height: 20,),
-
-            SizedBox(
-              width: 350,
-              height: 50,
-              child: signInState.signInStatus == SignInStatus.loading
-                ? Center(child: CircularProgressIndicator.adaptive())
-                : getCustomButton(
-                    buttonText: 'Sign in',
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        context.read<SignInCubit>().signInWithEmailAndPassword();
-                      }
-                    },
-                ),
-            ),
-          ],
+          ),
         ),
-      ),
     );
   }
 }
