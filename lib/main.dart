c49e482ca0b5a750e8a45201bac3b0f395cad3d9
akash <akash.punagin@gmail.com>
2021-11-17 11:41:07 +0530

    removed unused auth bloc

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:papers_for_peers/config/app_theme.dart';
 import 'package:papers_for_peers/data/repositories/auth/auth_repository.dart';
-import 'package:papers_for_peers/logic/blocs/auth/auth_bloc.dart';
 import 'package:papers_for_peers/logic/blocs/kud_notifications/kud_notifications_bloc.dart';
 import 'package:papers_for_peers/logic/cubits/app_theme/app_theme_cubit.dart';
 import 'package:papers_for_peers/logic/cubits/course_and_semester/course_and_semester_cubit.dart';
@@ -34,9 +33,6 @@ class _MyAppState extends State<MyApp> {
       ],
       child: MultiBlocProvider(
         providers: [
-          BlocProvider<AuthBloc>(
-            create: (context) => AuthBloc(authRepository: context.read<AuthRepository>()),
-          ),
           BlocProvider<AppThemeCubit>(
             create: (context) => AppThemeCubit(),
           ),
