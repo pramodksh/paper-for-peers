177843ecda21e6b26af9ce4ca36b2b83f7fc8cef
akash <akash.punagin@gmail.com>
2021-12-10 13:45:06 +0530

    added compression for pick

--- lib/data/repositories/file_picker/file_picker_repository.dart
+++ lib/data/repositories/file_picker/file_picker_repository.dart
@@ -1,12 +1,32 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf_compressor/pdf_compressor.dart';
 
 abstract class BaseFilePickerRepository {
   Future pickFile();
 }
 
 class FilePickerRepository extends BaseFilePickerRepository {
+
+  String getRandomString(int length) {
+    const _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+    Random _rnd = Random();
+    return String.fromCharCodes(Iterable.generate(
+        length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
+  }
+
+  Future<String> getTempPath() async {
+    var dir = await getExternalStorageDirectory();
+    await new Directory('${dir!.path}/CompressPdfs').create(recursive: true);
+
+    String randomString = getRandomString(10);
+    String pdfFileName = '$randomString.pdf';
+    return '${dir.path}/CompressPdfs/$pdfFileName';
+  }
+
 
   @override
   Future<File?> pickFile() async {
@@ -17,7 +37,17 @@ class FilePickerRepository extends BaseFilePickerRepository {
 
     if (result != null) {
       File file = File(result.files.single.path!);
-      return file;
+
+      print("SIZE: OLD: ${file.lengthSync()}");
+      String outputPath = await getTempPath();
+      try {
+        await PdfCompressor.compressPdfFile(file.path, outputPath, CompressQuality.MEDIUM);
+        print("SIZE: NEW: ${File(outputPath).lengthSync()}");
+        return File(outputPath);
+      } on Exception catch (e) {
+        return null;
+      }
+
     } else {
       return null;
     }
